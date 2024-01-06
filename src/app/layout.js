9a2dea9b46335f2bde7c8d51c9f3c73b69ce5882
mhamad raad <hamaraad883@gmail.com>
2024-01-06 17:53:35 +0300

    delete unnesunnecessary code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,6 @@
 import { Inter } from 'next/font/google';
 import './globals.css';
 
-import Banner from '@/components/Banner';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 
@@ -16,7 +15,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={inter.className}>
-        {/* <Banner /> */}
         <Header />
         {children}
         <Footer />
