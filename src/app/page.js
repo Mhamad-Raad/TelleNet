f8bfa42c54f9c23381b8feb5cd7f9062082a065c
mhamad raad <hamaraad883@gmail.com>
2024-01-13 20:45:37 +0300

    add structure for the about section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import AboutSection from '@/components/About/AboutSection';
 import Hero from '../components/HeroSection/Hero';
 
 const Home = () => {
   return (
     <main className='w-full h-[200vh] bg-bgColor'>
       <Hero />
+      <AboutSection />
     </main>
   );
 };
