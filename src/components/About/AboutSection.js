a80c4bdfc9afb250942de9b66b57e98133b891af
joker69216 <hamaraad883@gmail.com>
2024-01-13 22:57:11 +0300

    structure about us

--- src/components/About/AboutSection.js
+++ src/components/About/AboutSection.js
@@ -1,5 +1,10 @@
 const AboutSection = () => {
-  return <section>AboutSection</section>;
+  return <section className="flex flex-col">
+    <h2 className="">Get to Know Us</h2>
+    <div className="">
+
+    </div>
+  </section>;
 };
 
 export default AboutSection;
