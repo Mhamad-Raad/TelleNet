4aff9c5326009d556411f7361d201543bf5f2003
joker69216 <hamaraad883@gmail.com>
2024-01-13 23:29:42 +0300

    finalize today

--- src/components/About/AboutSection.js
+++ src/components/About/AboutSection.js
@@ -9,6 +9,7 @@ const AboutSection = () => {
       <p>
         <span className="font-bold">TelleNet<sub>Pro</sub></span> is guided by four principles: customer obsession rather than competitor focus, passion for invention, commitment to operational excellence, and long-term thinking. We are driven by the excitement of building technologies, inventing products, and providing services that change lives. We embrace new ways of doing things, make decisions quickly, and are not afraid to fail. We have the scope and capabilities of a large company, and the spirit and heart of a small one.
       </p>
+      
     </div>
   </section>;
 };
