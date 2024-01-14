0e8817a04aa13dcc87b900fb2cce76267d2d415b
mhamad raad <hamaraad883@gmail.com>
2024-01-14 19:16:45 +0300

    add about content

--- src/components/About/AboutSection.js
+++ src/components/About/AboutSection.js
@@ -1,17 +1,49 @@
+'use client';
+import { useState } from 'react';
+
 import LineSeperator from '.././UI/LineSeperator';
+import Globe from '../UI/Globe';
 
 const AboutSection = () => {
-  return <section className="flex flex-col gap-[10px]">
-    <h2 className="ml-[20px] text-textColor text-[28px] font-black">Get to Know Us</h2>
-    <LineSeperator />
-    
-    <div className="flex px-[10px] gap-[25px]">
-      <p>
-        <span className="font-bold">TelleNet<sub>Pro</sub></span> is guided by four principles: customer obsession rather than competitor focus, passion for invention, commitment to operational excellence, and long-term thinking. We are driven by the excitement of building technologies, inventing products, and providing services that change lives. We embrace new ways of doing things, make decisions quickly, and are not afraid to fail. We have the scope and capabilities of a large company, and the spirit and heart of a small one.
+  const [showText, setShowText] = useState(false);
+
+  const toggleShow = () => {
+    setShowText(!showText);
+  };
+
+  const aboutText = `is guided by four principles: customer obsession rather than competitor
+        focus, passion for invention, commitment to operational excellence, and
+        long-term thinking. We are driven by the excitement of building
+        technologies, inventing products, and providing services that change
+        lives. We embrace new ways of doing things, make decisions quickly, and
+        are not afraid to fail. We have the scope and capabilities of a large
+        company, and the spirit and heart of a small one.`;
+
+  return (
+    <section className='flex flex-col gap-[10px] items-center'>
+      <h2 className='text-textColor text-[28px] font-black'>Get to Know Us</h2>
+      {/* write me a short paragraph */}
+      <LineSeperator />
+      <p className='text-center mt-10 px-10'>
+        NetSpot provides cutting-edge technology solutions tailored to your
+        business needs, From cybersecurity to software development, trust our
+        experts to enhance your digital world.
       </p>
-      
-    </div>
-  </section>;
+      <Globe />
+      <p className='text-center mt-10 px-10'>
+        <span className='font-bold'>
+          TelleNet<sub>Pro</sub>
+        </span>{' '}
+        {showText ? aboutText : aboutText.slice(0, 300)}
+      </p>
+      <button
+        onClick={toggleShow}
+        className='text-textColor text-[18px] font-bold hover:underline'
+      >
+        {showText ? 'Show Less' : 'Show More'}
+      </button>
+    </section>
+  );
 };
 
 export default AboutSection;
--- src/components/UI/LineSeperator.js
+++ src/components/UI/LineSeperator.js
@@ -1,5 +1,7 @@
 const LineSeperator = () => {
-  return <div className='h-[2px] w-full bg-gradient-to-r from-[#ffffff29] via-bgSecondary to-[#ffffff29]' />
-}
+  return (
+    <div className='h-[2px] w-full bg-gradient-to-r from-[#ffffff29] via-bgSecondary to-[#ffffff29]' />
+  );
+};
 
 export default LineSeperator;
