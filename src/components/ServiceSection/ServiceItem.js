d3a82e7ecee4a49d8b835f6888f2564584e16c68
mhamad raad <hamaraad883@gmail.com>
2024-01-17 17:35:40 +0300

    add style to the first item

--- src/components/ServiceSection/ServiceItem.js
+++ src/components/ServiceSection/ServiceItem.js
@@ -1,7 +1,11 @@
 const ServiceItem = () => {
-  return <div className='flex flex-col gap-4'>
-    
-  </div>;
+  return (
+    <div className='w-[25%] h-[200px] p-1 rounded-md bg-gradient-to-br from-primary from-30% via-textColor via-50%  to-primary to-75%'>
+      <div className='w-full h-full flex flex-col rounded-md bg-bgSecondary'>
+
+      </div>
+    </div>
+  );
 };
 
 export default ServiceItem;
--- src/components/ServiceSection/ServicesSection.js
+++ src/components/ServiceSection/ServicesSection.js
@@ -6,7 +6,11 @@ import ServiceItem from './ServiceItem';
 const AboutSection = () => {
   return (
     <section className='w-full'>
-      <Element name='services' id='services' className='flex flex-wrap gap-10'>
+      <Element
+        name='services'
+        id='services'
+        className='flex flex-wrap gap-10 px-10'
+      >
         <ServiceItem />
       </Element>
     </section>
