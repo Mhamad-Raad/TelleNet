5b6cedbf4ba32848be42eec4d65e6b63a01c76b9
joker69216 <hamaraad883@gmail.com>
2024-01-17 20:02:43 +0300

    update the design of the servicesitem

--- src/components/ServiceSection/ServiceItem.js
+++ src/components/ServiceSection/ServiceItem.js
@@ -1,16 +1,13 @@
 import Image from 'next/image';
 import Layer from '@/assets/layer.png';
 
-import './ServiceItem.css'
-
-
 const ServiceItem = () => {
   return (
-    <div className='h-[200px] p-1 rounded-md bg-gradient-to-br from-primary from-30% via-textColor via-50%  to-primary to-75%'>
-      <div className='w-full h-full p-2 flex flex-col gap-2 rounded-md bg-bgSecondary'>
-        <Image src={Layer} className="img"/>
-        <h3 className="text-[22px] font-black text-textColor text-center">Composability</h3>
-        <p className="text-[18px] text-center text-textColor">
+    <div className='p-1 rounded-md bg-gradient-to-br from-bgSecondary from-30% via-primary via-50%  to-bgSecondary to-75%'>
+      <div className='w-full h-full px-3 py-7 flex flex-col items-center gap-2 rounded-md bg-bgColor'>
+        <Image src={Layer} className="w-[150px] h-[150px]"/>
+        <h3 className="text-[18px] mt-4 font-black text-textColor text-center">Composability</h3>
+        <p className="text-[16px] text-center text-textColor leading-[20px]">
           NetSpot's solutions let you build a flexible and adaptable business with unmatched agility and efficiency.
         </p>
       </div>
