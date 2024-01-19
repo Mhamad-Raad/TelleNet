91b8acae36019c37d92e687f8895a5d8147ef882
mhamad raad <hamaraad883@gmail.com>
2024-01-19 21:06:20 +0300

    add styles to the structure

--- src/components/ServiceSection/ServiceItem.js
+++ src/components/ServiceSection/ServiceItem.js
@@ -3,7 +3,7 @@ import Layer from '@/assets/layer.png';
 
 const ServiceItem = () => {
   return (
-    <div className='p-1 rounded-md bg-gradient-to-br from-bgSecondary from-30% via-primary via-50%  to-bgSecondary to-75%'>
+    <div className='max-w-[275px] p-1 rounded-md bg-gradient-to-br from-bgSecondary from-30% via-primary via-50%  to-bgSecondary to-75%'>
       <div className='w-full h-full px-3 py-7 flex flex-col items-center gap-2 rounded-md bg-bgColor'>
         <Image src={Layer} className="w-[150px] h-[150px]"/>
         <h3 className="text-[18px] mt-4 font-black text-textColor text-center">Composability</h3>
--- src/components/ServiceSection/ServicesSection.js
+++ src/components/ServiceSection/ServicesSection.js
@@ -4,7 +4,6 @@ import { Element } from 'react-scroll';
 import ServiceItem from './ServiceItem';
 import LineSeperator from '.././UI/LineSeperator';
 
-
 const AboutSection = () => {
   return (
     <section className='w-full'>
@@ -13,14 +12,17 @@ const AboutSection = () => {
         id='services'
         className='flex flex-col gap-[10px]'
       >
-        <h2 className="text-textColor text-[28px] font-black text-center">How Can We Help</h2>
+        <h2 className='text-textColor text-[28px] font-black text-center'>
+          How Can We Help
+        </h2>
         <LineSeperator />
-        <p className="text-center mt-1">
-          Experience exponential business growth with Netspot's services. Develop your business like never before with our innovative solutions and expert guidance.
+        <p className='text-center mt-1 phone:px-4'>
+          Experience exponential business growth with Netspot's services.
+          Develop your business like never before with our innovative solutions
+          and expert guidance.
         </p>
-        <div className="flex flex-wrap gap-10 mt-16">
-
-        <ServiceItem />
+        <div className='flex flex-wrap items-center justify-center gap-10 mt-16 phone:px-4'>
+          <ServiceItem />
         </div>
       </Element>
     </section>
