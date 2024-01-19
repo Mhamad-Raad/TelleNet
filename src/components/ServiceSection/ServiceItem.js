28e5095c0dc772ce245e80e7310301f484701571
mhamad raad <hamaraad883@gmail.com>
2024-01-19 21:51:51 +0300

    add responsivity to the section

--- src/components/ServiceSection/ServiceItem.js
+++ src/components/ServiceSection/ServiceItem.js
@@ -1,11 +1,9 @@
 import Image from 'next/image';
 import { useRef, useEffect } from 'react';
 
-import Layer from '@/assets/layer.png';
-
 import './style.css';
 
-const ServiceItem = () => {
+const ServiceItem = ({ ImageSrc, Title, Description }) => {
   const buttonRef = useRef(null);
 
   function mouseMoveEvent(e) {
@@ -24,18 +22,21 @@ const ServiceItem = () => {
   }, [buttonRef]);
 
   return (
-    <div className='max-w-[275px] p-1 rounded-md bg-gradient-to-br from-bgSecondary from-30% via-primary via-50%  to-bgSecondary to-75% ease-in duration-200 transission-transform hover:scale-105'>
+    <div className='phone:max-w-[275px] computer:max-w-[400px] p-1 rounded-md bg-gradient-to-br from-bgSecondary from-30% via-primary via-50%  to-bgSecondary to-75% ease-in duration-200 transission-transform hover:scale-105'>
       <div
-        className='w-full h-full px-3 py-7 flex flex-col items-center gap-2 rounded-md bg-bgColor shiny hover:cursor-none '
+        className='w-full h-full px-3 py-7 flex flex-col items-center gap-2 rounded-md bg-bgColor shiny hover:cursor-none'
         ref={buttonRef}
       >
-        <Image src={Layer} className='w-[150px] h-[150px]' alt='' />
-        <h3 className='text-[18px] mt-4 font-black text-textColor text-center'>
-          Composability
+        <Image
+          src={ImageSrc}
+          className='phone:w-[150px] phone:h-[150px] computer:w-[175px] computer:h-[175px]'
+          alt=''
+        />
+        <h3 className='phone:text-[18px] computer:text-[22px] mt-4 font-black text-primary text-center'>
+          {Title}
         </h3>
-        <p className='text-[16px] text-center text-textColor leading-[20px]'>
-          NetSpot's solutions let you build a flexible and adaptable business
-          with unmatched agility and efficiency.
+        <p className='phone:text-[16px] computer:text-[18px] text-center text-[#698097] phone:leading-[20px] computer:leading-[24px] computer:mt-4'>
+          {Description}
         </p>
       </div>
     </div>
--- src/components/ServiceSection/ServicesSection.js
+++ src/components/ServiceSection/ServicesSection.js
@@ -4,6 +4,8 @@ import { Element } from 'react-scroll';
 import ServiceItem from './ServiceItem';
 import LineSeperator from '.././UI/LineSeperator';
 
+import Layer from '@/assets/layer.png';
+
 const AboutSection = () => {
   return (
     <section className='w-full'>
@@ -16,13 +18,30 @@ const AboutSection = () => {
           How Can We Help
         </h2>
         <LineSeperator />
-        <p className='text-center mt-1 phone:px-4'>
+        <p className='text-center mt-1 phone:px-4 computer:text-[20px]'>
           Experience exponential business growth with Netspot's services.
           Develop your business like never before with our innovative solutions
           and expert guidance.
         </p>
         <div className='flex flex-wrap items-center justify-center gap-10 mt-16 phone:px-4'>
-          <ServiceItem />
+          <ServiceItem
+            ImageSrc={Layer}
+            Title='Composition'
+            Description="NetSpot's solutions let you build a flexible and adaptable business
+          with unmatched agility and efficiency."
+          />
+          <ServiceItem
+            ImageSrc={Layer}
+            Title='Composition'
+            Description="NetSpot's solutions let you build a flexible and adaptable business
+          with unmatched agility and efficiency."
+          />
+          <ServiceItem
+            ImageSrc={Layer}
+            Title='Composition'
+            Description="NetSpot's solutions let you build a flexible and adaptable business
+          with unmatched agility and efficiency."
+          />
         </div>
       </Element>
     </section>
