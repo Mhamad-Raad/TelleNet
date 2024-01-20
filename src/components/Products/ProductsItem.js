1cefeccc90f7c93286314643fccb8159b1c5bdee
mhamad raad <hamaraad883@gmail.com>
2024-01-20 20:30:01 +0300

    add new pop up animation

--- src/components/Products/ProductsItem.js
+++ src/components/Products/ProductsItem.js
@@ -1,9 +1,12 @@
 import Image from 'next/image';
-import { motion } from 'framer-motion';
-import Layers from '@/assets/layer.png';
-import './style.css';
+import { motion, useInView } from 'framer-motion';
+import { useRef } from 'react';
 
-const ProductsItem = () => {
+const ProductsItem = ({ ImageSrc, Title, Description, animation }) => {
+  const ref = useRef();
+  const isInView = useInView(ref);
+
+  let style = '';
   const containerVariants = {
     hidden: { opacity: 0 },
     visible: { opacity: 1 },
@@ -18,8 +21,20 @@ const ProductsItem = () => {
     hover: { scale: 1.1 },
   };
 
+  if (animation === 1) style = '100%';
+  else if (animation === 2) style = '1';
+
   return (
-    <div className='p-1 rounded-md bg-gradient-to-br from-bgSecondary from-30% via-primary via-50% to-bgSecondary to-75%'>
+    <motion.div
+      className='p-1 rounded-md bg-gradient-to-br from-bgSecondary from-30% via-primary via-50% to-bgSecondary to-75%'
+      style={{
+        width: animation === 1 ? (isInView ? style : '0') : '100%',
+        scale: animation === 2 ? (isInView ? style : '0') : '1',
+        opacity: isInView ? 1 : 0,
+        transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
+      }}
+      ref={ref}
+    >
       <motion.div
         className='rounded-md flex phone:flex-col tablet:flex-row items-center justify-between phone:p-4 tablet:p-10 laptop:p-20 phone:gap-8 transition-all duration-300'
         variants={containerVariants}
@@ -29,12 +44,10 @@ const ProductsItem = () => {
       >
         <div className='flex flex-col'>
           <h4 className='phone:text-[24px] tablet:text-[30px] computer:text-[38px] phone:mb-1'>
-            Product 1
+            {Title}
           </h4>
           <p className='phone:text-[14px] tablet:text-[16px] computer:text-[26px] phone:w-[100%] tablet:w-[90%] laptop:w-[75%] text-[#7a92b1]'>
-            Our innovative eGovernment Solutions can help you streamline
-            identity management, unleash the potential of OCR technology, and
-            enhance efficiency and security.
+            {Description}
           </p>
         </div>
 
@@ -44,10 +57,10 @@ const ProductsItem = () => {
           transition={{ duration: 0.3 }}
           className='phone:w-[75%] tablet:w-[33%]'
         >
-          <Image src={Layers} alt='' />
+          <Image src={ImageSrc} alt='' />
         </motion.div>
       </motion.div>
-    </div>
+    </motion.div>
   );
 };
 
--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -2,13 +2,38 @@
 
 import { Element } from 'react-scroll';
 
-import ProductsItem from './ProfuctsItem';
+import ProductsItem from './ProductsItem';
+
+import Layers from '@/assets/layer.png';
 
 const ProductsList = () => {
   return (
     <section className='phone:px-6 tablet:px-20'>
-      <Element name='products' id='products'>
-        <ProductsItem />
+      <Element name='products' id='products' className='flex flex-col gap-20'>
+        <ProductsItem
+          ImageSrc={Layers}
+          Title='Product 1'
+          Description='Our innovative eGovernment Solutions can help you streamline
+            identity management, unleash the potential of OCR technology, and
+            enhance efficiency and security.'
+          animation={1}
+        />
+        <ProductsItem
+          ImageSrc={Layers}
+          Title='Product 1'
+          Description='Our innovative eGovernment Solutions can help you streamline
+            identity management, unleash the potential of OCR technology, and
+            enhance efficiency and security.'
+          animation={2}
+        />
+        <ProductsItem
+          ImageSrc={Layers}
+          Title='Product 1'
+          Description='Our innovative eGovernment Solutions can help you streamline
+            identity management, unleash the potential of OCR technology, and
+            enhance efficiency and security.'
+          animation={3}
+        />
       </Element>
     </section>
   );
