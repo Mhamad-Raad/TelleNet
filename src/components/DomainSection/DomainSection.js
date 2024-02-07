777a499b4efff83806c6059fccf8ab0f538f222e
mhamad raad <hamaraad883@gmail.com>
2024-02-07 19:53:37 +0300

    update design of domain section

--- src/components/DomainSection/DomainSection.js
+++ src/components/DomainSection/DomainSection.js
@@ -1,8 +1,6 @@
 'use client';
 
 import { Element } from 'react-scroll';
-import { motion, useInView } from 'framer-motion';
-import { useRef } from 'react';
 
 import DomainItem from './DomainItem';
 import LineSeperator from '../UI/LineSeperator';
@@ -10,33 +8,13 @@ import LineSeperator from '../UI/LineSeperator';
 import Layer from '@/assets/layer.png';
 
 const AboutSection = () => {
-  const ref = useRef();
-  const isInView = useInView(ref, { once: true });
-
   return (
     <section className='w-full overflow-hidden'>
-      <Element
-        name='domains'
-        id='domains'
-        className='flex flex-col gap-[10px]'
-      >
+      <Element name='domains' id='domains' className='flex flex-col gap-[10px]'>
         <h2 className='text-textColor text-[28px] font-black text-center'>
           How Can We Help
         </h2>
         <LineSeperator />
-        <motion.p
-          className='text-center mt-1 phone:px-4 computer:text-[20px]'
-          ref={ref}
-          style={{
-            transform: isInView ? 'none' : 'translateY(100px)',
-            opacity: isInView ? 1 : 0,
-            transition: 'all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s',
-          }}
-        >
-          {`Experience exponential business growth with Netspot's services.
-          Develop your business like never before with our innovative solutions
-          and expert guidance.`}
-        </motion.p>
         <div className='flex flex-wrap items-center justify-center gap-10 mt-16 phone:px-4'>
           <DomainItem
             ImageSrc={Layer}
