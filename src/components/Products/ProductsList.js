e47faf680374d1ad0b060a616f56b47250bc16ad
mhamad raad <hamaraad883@gmail.com>
2024-02-12 22:07:02 +0300

    add structure

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -1,39 +1,67 @@
 'use client';
 
 import { Element } from 'react-scroll';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination, Navigation } from 'swiper/modules';
 
 import ProductsItem from './ProductsItem';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 import Layers from '@/assets/layer.png';
 
 const ProductsList = () => {
   return (
     <section className='phone:px-6 tablet:px-20 overflow-hidden'>
-      <Element name='products' id='products' className='flex flex-col gap-20'>
-        <ProductsItem
-          ImageSrc={Layers}
-          Title='Product 1'
-          Description='Our innovative eGovernment Solutions can help you streamline
-            identity management, unleash the potential of OCR technology, and
-            enhance efficiency and security.'
-          animation={1}
-        />
-        <ProductsItem
-          ImageSrc={Layers}
-          Title='Product 1'
-          Description='Our innovative eGovernment Solutions can help you streamline
-            identity management, unleash the potential of OCR technology, and
-            enhance efficiency and security.'
-          animation={2}
-        />
-        <ProductsItem
-          ImageSrc={Layers}
-          Title='Product 1'
-          Description='Our innovative eGovernment Solutions can help you streamline
-            identity management, unleash the potential of OCR technology, and
-            enhance efficiency and security.'
-          animation={3}
-        />
+      <Element name='products' id='products' className='flex flex-col gap-20 items-center'>
+        <div className='border border-[] w-[90%] h-[750px] p-12'>
+          <h2 className="text-center">Product title</h2>
+          <p className="">something long here</p>
+          <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className='mySwiper'
+        >
+          <SwiperSlide>
+            <div className="w-full">
+
+            <ProductsItem
+              ImageSrc={Layers}
+              Title='Product 1'
+              Description='Our innovative eGovernment Solutions can help you streamline
+              identity management, unleash the potential of OCR technology, and
+              enhance efficiency and security.'
+              animation={3}
+              />
+              </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div className="w-full">
+
+            <ProductsItem
+              ImageSrc={Layers}
+              Title='Product 1'
+              Description='Our innovative eGovernment Solutions can help you streamline
+              identity management, unleash the potential of OCR technology, and
+              enhance efficiency and security.'
+              animation={3}
+              />
+              </div>
+          </SwiperSlide>
+        </Swiper>
+        </div>
+        
       </Element>
     </section>
   );
