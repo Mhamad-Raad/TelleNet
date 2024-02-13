ad0e96524554d7af8582259fcf081d7ca416a1c7
mhamad raad <hamaraad883@gmail.com>
2024-02-13 20:37:40 +0300

    add products list

--- src/components/Products/ProductsList.js
+++ src/components/Products/ProductsList.js
@@ -14,60 +14,43 @@ import Layers from '@/assets/layer.png';
 const ProductsList = () => {
   console.log(Products);
   return (
-    <div className='border border-[] w-[90%] h-[750px] p-12'>
-      <h2 className='text-center'>IP ROUTERS</h2>
-      <p className='text-center'>
-        Our suite of IP routers encompasses a versatile range of solutions
-        tailored to meet specific networking requirements across various
-        industries. offering multiple routing technologies including IP/MPLS,
-        MPLS-TP, Segment routing, RSVP-TE, FlexE, and supporting a wide range of
-        services such as Ethernet, Layer 2 VPN, Layer 3 VPN, EVPN, Circuit
-        emulation, and XGS-PON. Beginning with our compact CPE customer premises
-        equipment, designed for applications like 4G and 5G Fronthaul, Critical
-        Infrastructure, and Enterprise use, these devices offer hardened
-        high-availability for mission-critical sectors while providing
-        cost-effective solutions for multiservice edge needs.
-      </p>
-      <Swiper
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        modules={[Autoplay, Pagination, Navigation]}
-        className='mySwiper'
-      >
-        <SwiperSlide>
-          <div className='w-full'>
-            <ProductsItem
-              ImageSrc={Layers}
-              Title='Product 1'
-              Description='Our innovative eGovernment Solutions can help you streamline
-              identity management, unleash the potential of OCR technology, and
-              enhance efficiency and security.'
-              animation={3}
-            />
+      Products.map((product, index) => {
+        return (
+          <div key={index} className='border border-[] w-[90%] h-[750px] p-12'>
+            <h2 className='text-center'>{product.title}</h2>
+            <p className='text-center'>{product.description}</p>
+            <Swiper
+              spaceBetween={30}
+              centeredSlides={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                clickable: true,
+              }}
+              navigation={true}
+              modules={[Autoplay, Pagination, Navigation]}
+              className='mySwiper'
+            >
+              {product.types.map((type, index) => {
+                return (
+                  <SwiperSlide key={index}>
+                    <div className='w-full'>
+                      <ProductsItem
+                        ImageSrc={type.image}
+                        Title={type.title}
+                        Description={type.description}
+                        animation={3}
+                      />
+                    </div>
+                  </SwiperSlide>
+                );
+              })}
+            </Swiper>
           </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className='w-full'>
-            <ProductsItem
-              ImageSrc={Layers}
-              Title='Product 1'
-              Description='Our innovative eGovernment Solutions can help you streamline
-              identity management, unleash the potential of OCR technology, and
-              enhance efficiency and security.'
-              animation={3}
-            />
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </div>
+        );
+      })
   );
 };
 
