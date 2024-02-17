35c78ddd2e9077535a14584a23a16490b3fd32c6
mhamad raad <hamaraad883@gmail.com>
2024-02-17 17:52:06 +0300

    add structure for services

--- src/components/SolutionsSection/SolutionsSection.js
+++ src/components/SolutionsSection/SolutionsSection.js
@@ -5,7 +5,7 @@ import LineSeperator from '../UI/LineSeperator';
 
 const SolutionsSection = () => {
   return (
-    <section className='flex flex-col items-center phone:px-4 laptop:px-8 computer:px-10'>
+    <section className='flex flex-col items-center phone:px-4 laptop:px-8 computer:px-10 overflow-hidden'>
       <h2 className='text-[36px] mb-2'>Solutions</h2>
       <LineSeperator />
       <SolutionsList />
