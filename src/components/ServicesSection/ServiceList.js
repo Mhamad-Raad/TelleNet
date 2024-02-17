ad75e15e2c43a664f242117eae04b058248e8365
mhamad raad <hamaraad883@gmail.com>
2024-02-17 21:44:31 +0300

    show the content

--- src/components/ServicesSection/ServiceList.js
+++ src/components/ServicesSection/ServiceList.js
@@ -1,5 +1,15 @@
+import ServiceItem from './ServiceItem';
+import Services from './Services';
+
 const ServiceList = () => {
-  return <div>ServiceList</div>;
+
+  return (
+    <div className='flex flex-col px-12 py-8 bg-[#0d1c2b] gap-4 w-full mt-4'>
+      {
+        Services.map((service,index) => <ServiceItem service={service} key={index} />)
+      }
+    </div>
+  );
 };
 
 export default ServiceList;
--- src/components/ServicesSection/ServicesSection.js
+++ src/components/ServicesSection/ServicesSection.js
@@ -1,26 +1,13 @@
-import ServiceItem from './ServiceItem';
+import ServiceList from './ServiceList';
+
 import LineSeperator from '../UI/LineSeperator';
 
-import Services from './Services';
-
 const ServicesSection = () => {
-  const service = Services[0];
-  console.log(Services);
   return (
     <section className='flex flex-col items-center px-12'>
       <h2 className='text-[32px] mb-2'>Our Services</h2>
       <LineSeperator />
-      <div className='flex flex-col px-12 py-8 bg-[#0d1c2b] gap-4 w-full mt-4'>
-        <ServiceItem service={service} />
-        <ServiceItem
-          service={service}
-                  />
-        <ServiceItem
-          service={service}
-          location='right-[-200px] top-[350px] z-[7]'
-        />
-        <ServiceItem service={service} />
-      </div>
+      <ServiceList />
     </section>
   );
 };
