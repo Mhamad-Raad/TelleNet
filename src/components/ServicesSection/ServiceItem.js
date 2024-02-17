5d2179c626ff36e904a72046775895909efe65e1
mhamad raad <hamaraad883@gmail.com>
2024-02-17 21:40:49 +0300

    update design

--- src/components/ServicesSection/ServiceItem.js
+++ src/components/ServicesSection/ServiceItem.js
@@ -1,5 +1,37 @@
-const ServiceItem = () => {
-  return <div>ServiceItem</div>;
+'use client';
+
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+import { IoIosArrowForward } from 'react-icons/io';
+
+const ServiceItem = ({ service }) => {
+  const [hovered, isHovered] = useState(false);
+
+  return (
+    <motion.div
+      className="bg-[#091420] phone:px-4 tablet:px-8 py-4 flex flex-col items-center rounded-2xl w-full min-h-[54px] h-[54px] overflow-hidden"
+      onMouseOver={() => isHovered(true)}
+      onMouseOut={() => isHovered(false)}
+      whileHover={{
+        height: 'auto',
+      }}
+      transition={{ ease: 'linear', duration: 0.3 }}
+    >
+      <div className='flex justify-between items-center text-[20px] w-full mb-4'>
+        <h2>{service.title}</h2>
+        <motion.div
+          className='text-[28px]'
+          style={{
+            transform: hovered ? 'rotate(90deg)' : 'rotate(0deg)',
+          }}
+        >
+          <IoIosArrowForward />
+        </motion.div>
+      </div>
+      <p>{service.description}</p>
+    </motion.div>
+  );
 };
 
 export default ServiceItem;
--- src/components/ServicesSection/ServicesSection.js
+++ src/components/ServicesSection/ServicesSection.js
@@ -1,16 +1,25 @@
+import ServiceItem from './ServiceItem';
 import LineSeperator from '../UI/LineSeperator';
 
 import Services from './Services';
 
 const ServicesSection = () => {
+  const service = Services[0];
   console.log(Services);
   return (
     <section className='flex flex-col items-center px-12'>
       <h2 className='text-[32px] mb-2'>Our Services</h2>
       <LineSeperator />
-      <div className='border w-[650px] h-[650px] rounded-full mt-10 relative flex items-center justify-center'>
-        <div className='border w-[75%] h-[75%] rounded-full absolute' />
-        <div className='border w-[50%] h-[50%] rounded-full absolute' />
+      <div className='flex flex-col px-12 py-8 bg-[#0d1c2b] gap-4 w-full mt-4'>
+        <ServiceItem service={service} />
+        <ServiceItem
+          service={service}
+                  />
+        <ServiceItem
+          service={service}
+          location='right-[-200px] top-[350px] z-[7]'
+        />
+        <ServiceItem service={service} />
       </div>
     </section>
   );
