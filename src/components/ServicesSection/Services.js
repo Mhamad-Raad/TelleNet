bc3a24cbe9b604b1014adcfc534bc3476871fb28
mhamad raad <hamaraad883@gmail.com>
2024-02-25 22:59:26 +0300

    delete dublicate data

--- src/components/ServicesSection/Services.js
+++ src/components/ServicesSection/Services.js
@@ -1,47 +1,32 @@
-import Layer from '@/assets/layer.png';
-
 const Services = [
   {
     title: "Telenetpro's Comprehensive Service Offerings",
     description:
       'At Telenetpro, we recognize that the successful implementation and sustained operation of cutting-edge communication solutions require more than just innovative technology. Our dedicated services teams are poised to guide you through every step of your network transformation journey, ensuring you leverage the maximum value from your solution. From deployment and integration to ongoing maintenance, monitoring, and education, our holistic approach aligns with your business objectives.',
-    image: Layer,
   },
   {
     title: 'Professional Services for Network Transformation',
     description:
       'At Telenetpro Communications, we understand the challenges that service providers and enterprises face in deploying and maintaining IP-based voice, multimedia, and IP Optical communications solutions. Our Professional Services team is dedicated to supporting our industry-leading solutions throughout their lifecycle, from deployment and integration to ongoing maintenance and monitoring.',
-    image: Layer,
   },
   {
     title: 'Expert Deployment Services',
-    description:
-      `Our deployment services encompass end-to-end support to ensure your network reaches its full potential. Whether you're planning a network expansion project, upgrading hardware and software, or executing program-specific deployments, our team of experts provides seamless project management and logistics support to oversee every aspect of the deployment process.`,
-    image: Layer,
-  },
-  {
-    title: 'Expert Deployment Services',
-    description:
-      "Our deployment services encompass end-to-end support to ensure your network reaches its full potential. Whether you're planning a network expansion project, upgrading hardware and software, or executing program-specific deployments, our team of experts provides seamless project management and logistics support to oversee every aspect of the deployment process.",
-    image: Layer,
+    description: `Our deployment services encompass end-to-end support to ensure your network reaches its full potential. Whether you're planning a network expansion project, upgrading hardware and software, or executing program-specific deployments, our team of experts provides seamless project management and logistics support to oversee every aspect of the deployment process.`,
   },
   {
     title: 'Tailored Network Modernization',
     description:
       'As a recognized global leader in Network Transformation, we offer tailored network modernization services to help you accelerate your return on investment, optimize operational capability, and enhance network performance and health. From planning and design to deployment and integration, we provide customized solutions to meet your specific needs and goals.',
-    image: Layer,
   },
   {
     title: 'Maintenance Support',
     description:
       'Our Maintenance Support services offer proactive monitoring to keep your production communications running smoothly, allowing you to focus on running your business. With a comprehensive support strategy for all products, applications, and solutions purchased, we ensure that any issues are identified, addressed, and rectified quickly, providing you with peace of mind and confidence in your network infrastructure.',
-    image: Layer,
   },
   {
     title: 'Professional Consulting and Education Services',
     description:
       'We provide consulting and education services to help you transform your business, improve processes, and streamline operations. Our team of experts works closely with you to turn your network goals into actionable plans, while our education services ensure that you have the technical knowledge and skills necessary to achieve service readiness and delivery goals, accelerating time-to-market and maximizing the value of your products and solutions.',
-    image: Layer,
   },
 ];
 
